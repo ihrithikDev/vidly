1277f60c474ba6075e0cc8d5ac1c43c66ef47bf4
“Hrithik <ihrithikdev@gmail.com>
2020-11-22 21:58:19 +0530

    Adding componennt and Services

---
+++ src/components/movies.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  };
+  handleDelete = (movie) => {
+    // console.log(movie);
+    const movies = this.state.movies.filter((m) => {
+      // console.log(m._id, movie._id);
+      return m._id !== movie._id;
+    });
+    this.setState({ movies: movies });
+  };
+  render() {
+    return (
+      <>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    className="btn btn-danger btn-sm"
+                    onClick={() => this.handleDelete(movie)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default Movies;
